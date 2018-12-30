5ab0169749646460b06dd5ef26326147765fb1d4
robertbucurbtr <robertbucurbtr@users.noreply.github.com>
2018-12-30 12:17:51 +0200

    initial commit

---
+++ Players/Models/Player.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Players.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string NickName { get; set; }
+    }
+}
